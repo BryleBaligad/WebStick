0c9f632c0372dcc6a2de68652b4dd589d238d222
Bryle Baligad (laptop-win) <brylematthewbaligad.fernandez@gmail.com>
2024-01-30 11:14:52 +0800

    Update init script to use compressed node

--- init.ps1
+++ init.ps1
@@ -11,7 +11,10 @@ Write-Output ""
 Write-Output "WebStick $($json.version)  -  (c) Bryle Baligad, 2023 - 2024"
 if ((Test-Path .\node.exe) -eq $false) {
     Write-Output "Downloading prerequisite (node.js v16.20.2)"
-    Invoke-WebRequest http://local.brylebaligad.com/public/dist/WebStick/0.0.1/node.exe -OutFile node.exe
+    Invoke-WebRequest http://local.brylebaligad.com/public/dist/WebStick/0.0.1/node.zip -OutFile node.zip
+    Write-Output "Unpacking prerequisite (node.js v16.20.2)"
+    Expand-Archive -Path .\node.zip -DestinationPath .\
+    Remove-Item .\node.zip
     if ((Test-Path .\node_modules) -eq $false) {
         Write-Output "Downloading prerequisite (node_modules)"
         Invoke-WebRequest http://local.brylebaligad.com/public/dist/WebStick/0.0.1/node_modules.zip -OutFile node_modules.zip

c07f3ebe1dff0df6d4ece7f11bfbfb99a0eb7467
Bryle Baligad (laptop-win) <brylematthewbaligad.fernandez@gmail.com>
2025-06-13 23:12:15 +0800

    Update init script to check for node_modules folder separately Update copyright year

--- init.ps1
+++ init.ps1
@@ -15,13 +15,14 @@ if ((Test-Path .\node.exe) -eq $false) {
     Write-Output "Unpacking prerequisite (node.js v16.20.2)"
     Expand-Archive -Path .\node.zip -DestinationPath .\
     Remove-Item .\node.zip
-    if ((Test-Path .\node_modules) -eq $false) {
-        Write-Output "Downloading prerequisite (node_modules)"
-        Invoke-WebRequest http://local.brylebaligad.com/public/dist/WebStick/0.0.40/node_modules.zip -OutFile node_modules.zip
-        Write-Output "Unpacking prerequisite (node_modules)"
-        Expand-Archive -Path .\node_modules.zip -DestinationPath .\node_modules
-        Remove-Item .\node_modules.zip
-    }
+}
+
+if ((Test-Path .\node_modules) -eq $false) {
+    Write-Output "Downloading prerequisite (node_modules)"
+    Invoke-WebRequest http://local.brylebaligad.com/public/dist/WebStick/0.0.40/node_modules.zip -OutFile node_modules.zip
+    Write-Output "Unpacking prerequisite (node_modules)"
+    Expand-Archive -Path .\node_modules.zip -DestinationPath .\node_modules
+    Remove-Item .\node_modules.zip
 }
 
 Clear-Host
